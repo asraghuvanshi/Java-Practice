ccad9d9fc95b0243431dd04218aed98ed6728ea9
asraghuvanshi <asraghuvanshi52@.com>
2023-01-22 23:10:08 +0530

    ArrayList implementation

---
+++ ArrayListIClass.java
@@ -0,0 +1,43 @@
+import java.util.ArrayList;
+
+class ArrayListClass{
+
+
+    static void reverseArrayList(ArrayList<Integer> list){
+    
+    int i = 0 , j = list.size() - 1;
+    while(i < j){
+       
+        Integer temp = Integer.valueOf(list.get(i));
+        list.set(i, list.get(j));
+        list.set(j, temp);
+        System.out.println("temp: " + temp);
+        j--;
+        i++;
+    }
+    System.out.println("Reserse List is : "+ list);
+    }
+    public static void main(String args[]){
+        
+        ArrayList<Integer> list = new ArrayList<>(); 
+        // ArrayList list = new ArrayList();
+        list.add(1);    
+        list.add(2);
+        list.add(3);
+        list.add(4);
+
+        if (list.contains(Integer.valueOf(4))){
+            System.out.println("Element is present");
+        }
+        else{
+            System.out.println("Element not present");
+        }
+
+        // list.remove(3);
+        // list.set(1 ,12); 
+        System.out.println("arraylist "+ list);
+        System.out.println("arraylist size "+ list.size());
+
+        reverseArrayList(list);
+    }
+}
