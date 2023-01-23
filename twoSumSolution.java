35ec6f6db19c0eaf1ef98846045310e544b6c0cc
asraghuvanshi <asraghuvanshi52@.com>
2023-01-23 23:55:31 +0530

    updated logics

--- twoSumSolution.java
+++ twoSumSolution.java
@@ -1,23 +1,25 @@
 
 class Solution {
     public static int[] twoSum(int[] nums, int target) {
-        int newArray[]  = new int[nums.length];
+        int newArray[]={ 0 , 0};
 
         for(int i = 0 ; i<nums.length ; i++){
-            for(int j  =  1; j<nums.length ; j++){
+            for(int j  =  1 + i; j<nums.length ; j++){
                if(nums[i] + nums[j] == target){
-                    System.out.println(" "+ nums[i] +" "+ nums[j]+"target"+ target +"nums");
-                    newArray[i] = newArray[i , j];
-               }
+                    newArray[0] = i;
+                    newArray[1] = j;
+                    System.out.println(java.util.Arrays.toString(newArray));               }
             }
         }
         return newArray;
     }
 
+
      public static void main(String[] args) {
-		int arr[] = {1 ,2 ,3, 4, 5};
+	int arr[] = {1 ,2 ,3, 4, 5};
         int target = 3;
         twoSum(arr , target);
         System.out.println(twoSum(arr , target));
 	}
 }
+
