94f952dd9179e92713598278e9f80d61000856d5
A.S Raghuvanshi <110293033+asraghuvanshi@users.noreply.github.com>
2023-01-29 20:13:36 +0530

    Update Digit_Counter.java

--- Digit_Counter.java
+++ Digit_Counter.java
@@ -10,7 +10,7 @@ Total number of count 1234567: = 7
 */
 
 import java.util.Scanner;
-public class Program
+public class DigitCounter
 {
     public static void main(String[] args) {
 	Scanner sc = new Scanner(System.in);
