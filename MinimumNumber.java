02756e4bf3bd20c7536ad1c9ad6807db2acc4c32
programmer <asraghuvanshi52@gmail.com>
2023-03-02 00:24:58 +0530

    minimum and second largest number in array

--- MinimumNumber.java
+++ MinimumNumber.java
@@ -1,21 +1,33 @@
+import java.util.Arrays;
+
 public class MinimumNumber
 {
     public static int findMinimum(int arr[]){
         int min = arr[0];
         for( int i = 1; i<arr.length; i++){
-           if(arr[i] > min){
+           if(arr[i] < min){
             min = arr[i];
            }
         }
         return min;
     }
-    public static void main(String[] args) {
-		
-        int arr[] = {5, 2 ,1, 6, 7, 8 , 25 , 12};
-        System.out.println("Size of array is :"+ arr.length);
-        int minArr = MinimumNumber.findMinimum(arr);
-        System.out.println(minArr);
 
+    public static void findSecondLargest(int arr[]){
+        Arrays.sort(arr);
+        for(int i = arr.length - 2; i >= 0; --i){
+            if (arr[i] != arr[arr.length - 1])
+            {
+                System.out.println("Second largest number is :" + arr[i]);
+            }
+        }
+    }
+        int arr[] = {5, 2 ,1, 6, 7, 8 , 25 , 12};
+        for (int i =0 ; i< arr.length ; i++) {
+            System.out.print(arr[i]+ " ");
+        }
+        int minArr = MinimumNumber.findMinimum(arr);
+        System.out.println("\nMinimum number in array is : " + minArr);
+        MinimumNumber.findSecondLargest(arr);
 
 	}
 }
